26df8de8dbf1d41e783c1f2eaccee6c3ea4c9013
denis luft <denisluft8@gmail.com>
2022-07-14 21:14:06 -0300

    Implementado reutilização da tabela

--- src/mock/products.js
+++ src/mock/products.js
@@ -1,19 +1,19 @@
 const productsArr = [
-    {
-      product: 'Coca Cola',
-      company: 'Coca Cola',
-      price: 'R$10,00',
-    },
-    {
-      product: 'Fanta Laranja',
-      company: 'Coca Cola',
-      price: 'R$08,00',
-    },
-    {
-      product: 'Kuat',
-      company: 'Coca Cola',
-      price: 'R$06,00',
-    },
+  {
+    name: 'Coca Cola',
+    company: 'Coca Cola',
+    price: 'R$10,00'
+  },
+  {
+    name: 'Fanta Laranja',
+    company: 'Coca Cola',
+    price: 'R$08,00'
+  },
+  {
+    name: 'Kuat',
+    company: 'Coca Cola',
+    price: 'R$06,00'
+  }
 ]
 
-export {productsArr}
+export { productsArr }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,19 @@
-import Header from "../components/Header";
-import UserTable from "../components/UserTable";
-import { userArr } from "../mock/users";
-import { products, productsArr } from "../mock/products";
+import Header from '../components/Header'
+import Table from '../components/Table'
+import { userArr } from '../mock/users'
+import { productsArr } from '../mock/products'
 
 const Home = () => {
-  const headerUser = ["Name", "Email", "Occupation", "Birthday",]
-  const headerProduct = ["Product", "Company", "Price",]
+  const headerUser = ['Name', 'Email', 'Occupation', 'Birthday']
+  const headerProduct = ['Product', 'Company', 'Price']
 
   return (
     <>
       <Header />
-      <UserTable type="user" headerData={headerUser} bodyData={userArr} />
-      <UserTable type="product" headerData={headerProduct} bodyData={productsArr} />
+      <Table type="user" headerData={headerUser} bodyData={userArr} />
+      <Table type="product" headerData={headerProduct} bodyData={productsArr} />
     </>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
